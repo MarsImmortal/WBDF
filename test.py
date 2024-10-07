e672e26804d4c385bb393e41568557a3cb85cd35
MarsImmortal <s222198007@deakin.edu.au>
2024-10-07 14:55:54 +1100

    modify the elr_dnn_cin function by using keras func

--- test.py
+++ test.py
@@ -366,79 +366,62 @@ def sample_synthetic_data(weights, kdb_high_order_encoder, y_counts, ohe=True, s
 def broad(input_dim, output_dim, constraint):
     return Dense(output_dim, input_dim=input_dim, activation='softmax', kernel_constraint=constraint)
 
-
 def elr_dnn_cin(elr_constrains, broad_units, dnn_feature_columns, output_units, dnn_hidden_units=(256, 256),
                 cin_layer_size=(128, 128,), cin_split_half=True, cin_activation='relu', l2_reg_linear=0.00001,
                 use_fm=False, fm_group=None,
                 l2_reg_embedding=0.00001, l2_reg_dnn=0, l2_reg_cin=0, seed=1024, dnn_dropout=0,
                 dnn_activation='relu', dnn_use_bn=False, task='binary'):
-    """	Instantiates the xDeepFM architecture.
-
-    :param linear_feature_columns: An iterable containing all the features used by linear part of the model.
-    :param dnn_feature_columns: An iterable containing all the features used by deep part of the model.
-    :param dnn_hidden_units: list,list of positive integer or empty list, the layer number and units in each layer of deep net
-    :param cin_layer_size: list,list of positive integer or empty list, the feature maps  in each hidden layer of Compressed Interaction Network
-    :param cin_split_half: bool.if set to True, half of the feature maps in each hidden will connect to output unit
-    :param cin_activation: activation function used on feature maps
-    :param l2_reg_linear: float. L2 regularizer strength applied to linear part
-    :param l2_reg_embedding: L2 regularizer strength applied to embedding vector
-    :param l2_reg_dnn: L2 regularizer strength applied to deep net
-    :param l2_reg_cin: L2 regularizer strength applied to CIN.
-    :param seed: integer ,to use as random seed.
-    :param dnn_dropout: float in [0,1), the probability we will drop out a given DNN coordinate.
-    :param dnn_activation: Activation function to use in DNN
-    :param dnn_use_bn: bool. Whether use BatchNormalization before activation or not in DNN
-    :param task: str, ``"binary"`` for  binary logloss or  ``"regression"`` for regression loss
-    :return: A Keras model instance.
-    """
-
+    """Instantiates the xDeepFM architecture."""
+    
+    # Build input features
     features = build_input_features(dnn_feature_columns)
     broad_inputs = Input((broad_units,), name='broad')
 
+    # Prepare input list
     inputs_list = list(features.values())
     inputs_list.insert(0, broad_inputs)
 
+    # Compute broad output
     broad_output = broad(broad_units, output_units, elr_constrains)(broad_inputs)
 
+    # Embedding for CIN and DNN
     sparse_embedding_list, dense_value_list = input_from_feature_columns(features, dnn_feature_columns,
                                                                          l2_reg_embedding, seed)
     cin_input = concat_func(sparse_embedding_list, axis=1)
 
+    # DNN part
     dnn_input = combined_dnn_input(sparse_embedding_list, dense_value_list)
     dnn_output = DNN(dnn_hidden_units, dnn_activation, l2_reg_dnn, dnn_dropout, dnn_use_bn, seed=seed)(dnn_input)
-    dnn_output = tf.keras.layers.Dense(
-        output_units, use_bias=False, kernel_initializer=tf.keras.initializers.glorot_normal(seed))(dnn_output)
-    broad_output = tf.convert_to_tensor(broad_output, dtype=tf.float32)
-    dnn_output = tf.convert_to_tensor(dnn_output, dtype=tf.float32) 
-    output = tf.concat([broad_output, dnn_output], axis=-1)
+    dnn_output = tf.keras.layers.Dense(output_units, use_bias=False, kernel_initializer=tf.keras.initializers.glorot_normal(seed))(dnn_output)
 
+    # Concatenate broad output and DNN output
+    output = tf.keras.layers.Concatenate(axis=-1)([broad_output, dnn_output])
+
+    # CIN part
     if len(cin_layer_size) > 0:
-        exFM_out = CIN(cin_layer_size, cin_activation,
-                       cin_split_half, l2_reg_cin, seed)(cin_input)
-        exFM_logit = tf.keras.layers.Dense(output_units, kernel_initializer=tf.keras.initializers.glorot_normal(seed))(
-            exFM_out)
-        output = tf.concat([output, exFM_logit], axis=-1)
+        exFM_out = CIN(cin_layer_size, cin_activation, cin_split_half, l2_reg_cin, seed)(cin_input)
+        exFM_logit = tf.keras.layers.Dense(output_units, kernel_initializer=tf.keras.initializers.glorot_normal(seed))(exFM_out)
+        output = tf.keras.layers.Concatenate(axis=-1)([output, exFM_logit])
 
-    if use_fm == True:
+    # FM part
+    if use_fm:
         from deepctr.layers.interaction import FM
         from deepctr.feature_column import DEFAULT_GROUP_NAME
         if fm_group is None:
             fm_group = [DEFAULT_GROUP_NAME]
-        group_embedding_dict, _ = input_from_feature_columns(features, dnn_feature_columns, l2_reg_embedding,
-                                                             seed, support_group=True)
-        #with open('../data/group_embedding_dict.txt', 'w') as f:
-            #dump(group_embedding_dict, f)
+        group_embedding_dict, _ = input_from_feature_columns(features, dnn_feature_columns, l2_reg_embedding, seed, support_group=True)
 
-        fm_logit = add_func([FM()(concat_func(v, axis=1))
-                             for k, v in group_embedding_dict.items() if k in fm_group])
+        fm_logit = add_func([FM()(concat_func(v, axis=1)) for k, v in group_embedding_dict.items() if k in fm_group])
+        output = tf.keras.layers.Concatenate(axis=-1)([output, tf.reshape(fm_logit, (-1, 1))])
 
-        output = tf.concat([output, tf.reshape(fm_logit, (-1, 1))], axis=-1)
-
+    # Final DNN layers and output
     output = DNN([100, 50], dnn_activation, l2_reg_dnn, dnn_dropout, dnn_use_bn, seed=seed)(output)
-    output = tf.keras.layers.Dense(
-        1, use_bias=False, kernel_initializer=tf.keras.initializers.glorot_normal(seed))(dnn_output)
+    output = tf.keras.layers.Dense(1, use_bias=False, kernel_initializer=tf.keras.initializers.glorot_normal(seed))(output)
+
+    # Apply prediction layer
     output = PredictionLayer(task)(output)
 
+    # Build model
     model = tf.keras.models.Model(inputs=inputs_list, outputs=output)
     return model
 
