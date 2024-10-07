326cedd48182cfd5edb16d6988469cd3b075bf12
MarsImmortal <s222198007@deakin.edu.au>
2024-10-07 13:43:45 +1100

    modify import statement removing .python

--- deepctr/layers/sequence.py
+++ deepctr/layers/sequence.py
@@ -10,7 +10,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.python.keras import backend as K
 from tensorflow.python.keras.initializers import TruncatedNormal
-from tensorflow.python.keras.layers import LSTM, Lambda, Layer
+from tensorflow.keras.layers import LSTM, Lambda, Layer, Dropout
 
 from .core import LocalActivationUnit
 from .normalization import LayerNormalization
